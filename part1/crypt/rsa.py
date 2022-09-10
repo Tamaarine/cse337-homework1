5c7c94c98d56ecf61bf4f13dfc568efa01216429
Tamaarine <irebootplaygt@gmail.com>
2022-09-10 15:56:50 -0400

    Finished implementing rsa class. init takes in p, encrypt takes in message m that's hashed already and return the encrypted ciphertext and public key. decrypt takes in ciphertext and public key to decrypt the message

--- part1/crypt/rsa.py
+++ part1/crypt/rsa.py
@@ -1,15 +1,42 @@
 import random as rd
 # import the mymath module here.
+from mymath import mymath
 
 class Rsa:
     # initialize to set p, q, and n.
-    def __init__():
-        pass
+    def __init__(self, p):
+        # p is provided
+        # n = p * q
+        self.p = p
+        self.q = p + 1000
+        self.n = self.p * self.q
 
     # generates a cipher string for a message m
-    def encrypt():
-        pass
+    def encrypt(self, m):
+        '''
+        message m, is hashed
+        
+        Return both the cipher text, and the public key generated
+        '''
+        K = mymath.lcm(self.p - 1, self.q - 1)
+        e = mymath.pubkExp(K) # Generate e, public key
+        
+        c = (m ** e) % self.n
+        
+        return c, e
 
     # decrypts a cipher string to get back original message
     def decrypt(self, c, e):
-        pass
+        '''
+        c, is the ciphertext
+        e, is the public key
+        
+        Decrypt the ciphertext using e 
+        '''
+        K = mymath.lcm(self.p - 1, self.q - 1)
+        
+        d = mymath.prikExp(K, e)
+        
+        m = (c ** d) % self.n
+        
+        return m
