e10d81fb213faae83e8ebf8104fb8cead7c24529
Tamaarine <irebootplaygt@gmail.com>
2022-09-10 13:16:26 -0400

    Finished hash, and finished gcd

--- part1/mymath/mymath.py
+++ part1/mymath/mymath.py
@@ -4,7 +4,11 @@ def isPrime(n):
     pass
 
 def gcd(a,b):
-    pass
+    # euclidean's algorithm for finding gcd.
+    if a == 0 or b == 0:
+        # gcd(0, 3) = 3, gcd(3, 0) = 3. Return whichever is the max
+        return max(a, b)
+    return gcd(b, a % b)
 
 def lcm(a,b):
     pass
@@ -19,4 +23,7 @@ def prikExp(x, y):
 
 # Returns the hash of a string message. Sum of its ascii characters.
 def hash(s):
-    pass
+    sum = 0
+    for letter in s:
+        sum += ord(letter)
+    return sum
