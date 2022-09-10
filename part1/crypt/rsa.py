a8e4632e7273479c65f78289a3abf4fbe97991f4
Tamaarine <irebootplaygt@gmail.com>
2022-09-10 18:16:19 -0400

    Using built-in pow function to do fast exponentiation

--- part1/crypt/rsa.py
+++ part1/crypt/rsa.py
@@ -14,14 +14,14 @@ class Rsa:
     # generates a cipher string for a message m
     def encrypt(self, m):
         '''
-        message m, is hashed
+        message m, is hashed already
         
         Return both the cipher text, and the public key generated
         '''
         K = mymath.lcm(self.p - 1, self.q - 1)
         e = mymath.pubkExp(K) # Generate e, public key
         
-        c = (m ** e) % self.n
+        c = pow(m, e, self.n)
         
         return c, e
 
@@ -31,12 +31,13 @@ class Rsa:
         c, is the ciphertext
         e, is the public key
         
-        Decrypt the ciphertext using e 
+        Decrypt the ciphertext using e and return the message
+        that is decrypted
         '''
         K = mymath.lcm(self.p - 1, self.q - 1)
         
         d = mymath.prikExp(K, e)
         
-        m = (c ** d) % self.n
+        m = pow(c, d, self.n)
         
         return m
--- part2/main.py
+++ part2/main.py
@@ -1,5 +1,7 @@
 import sys, os
 sys.path.insert(1, os.getcwd())
+
+print(sys.path)
 from parser.parse import parseArgs
 
 args = sys.argv
